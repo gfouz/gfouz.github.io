ace41c3a25eceb24683c5299eee7baf5e4c012d1
gfouz <gfouz1975@gmail.com>
2021-08-13 06:49:16 +0200

    nuxt.config.js modified

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,8 +4,8 @@ export default {
 
  
   router: {
-     base: 'gfouz.github.io'
-   },
+            base: 'gfouz.github.io'
+    },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
